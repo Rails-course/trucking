42e90ecfefbcf146a99c6bdef24cdbb7dd390917
NDragneelL9 <nell.dragneel@gmail.com>
2022-02-01 17:06:45 +0300

    US-1.2. remove redundant files/links

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,7 +20,3 @@ console.log('Hello World from Webpacker')
 const componentRequireContext = require.context("components", true);
 const ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
-// Support component names relative to this directory:
-var componentRequireContext = require.context("components", true);
-var ReactRailsUJS = require("react_ujs");
-ReactRailsUJS.useContext(componentRequireContext);
