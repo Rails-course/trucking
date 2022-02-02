a1240f0ee87cb62e82baba8e56f5095061a652e1
Catherine <kateiiverem@gmail.com>
2022-02-02 16:40:50 +0300

    feature: add remember device of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@
 
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  # :confirmable, :lockable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :timeoutable,
+         :validatable
 
-  validates :email, presence: true, length: {minimum: 10, maximum: 100}
-  validates :password, presence: true, length: {minimum: 6, maximum: 100}
+  validates :email, presence: true, length: { minimum: 10, maximum: 100 }
+  validates :password, presence: true, length: { minimum: 6, maximum: 100 }
 end
