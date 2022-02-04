2d26fd394573aeb394a16ffe66fbfb51f941832f
Catherine <kateiiverem@gmail.com>
2022-02-04 13:50:04 +0300

    US-1.2 Log in. Add sign up page with style

--- app/javascript/packs/index.tsx
+++ app/javascript/packs/index.tsx
@@ -1,11 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../components/App';
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-      <App/>,
-      document.getElementById("root")
-  );
-});
