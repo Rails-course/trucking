9a7e930c6f4cbdebf347d7e812fb3abd67581a55
NDragneelL9 <nell.dragneel@gmail.com>
2022-02-07 20:05:22 +0300

    TS-2.Roles table. Created role model with association to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  belongs_to :role
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :timeoutable,
