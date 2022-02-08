42578a219a1d79c04a044237979504cf0678c708
NDragneelL9 <nell.dragneel@gmail.com>
2022-02-08 21:25:08 +0300

    US-2.6.Create company users. Add routes to create users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users
   root 'pages#home'
-  get '/clients',to: 'users#index'
+  get '/clients', to: 'users#index'
+
+  scope '/users' do
+    get '/new', to: 'pages#new_user'
+    post '/create', to: 'pages#create_user'
+  end
   # match '*path', to: 'pages#home', via: :all
 end
