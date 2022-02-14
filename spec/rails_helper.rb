ddff34779dc3790651037146f151e457240cb305
helmy-er <helbmerliuni@gmail.com>
2022-02-14 21:28:27 +0300

    US-2.3 Remove Company. Refactor specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,7 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  config.include Warden::Test::Helpers
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
--- spec/requests/companies_spec.rb
+++ spec/requests/companies_spec.rb
@@ -1,20 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "Companies", type: :request do
-  include Warden::Test::Helpers
+  let(:company) { create(:company) }
   before(:each) do
-    Warden.test_mode!
-    user = instance_double(User, to_key: 1, authenticatable_salt: 'example')
-    login_as(create(:user), scope: 'user')
+    login_as(create(:user))
   end
   describe "DELETE /companies" do
     it 'valid delete' do
-      company=create(:company)
       delete "/companies/#{company.id}"
       expect(response).to have_http_status(204)
     end
     it 'invalid delete' do
-      company=create(:company)
       delete "/companies/#{company.id+1}"
       expect(response).to have_http_status(404)
   end
