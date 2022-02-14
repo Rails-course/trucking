c8de6620fe120ec152aecb7fe177515525b4d8e9
helmy-er <helbmerliuni@gmail.com>
2022-02-14 21:56:35 +0300

    US-2.3 Remove Company. Return TODO

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  # TODO: this factory should be updated when additional fields will be added
   factory :user do
     email { 'test@test.com' }
     password { 'password1' }
