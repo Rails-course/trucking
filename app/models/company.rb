449655fa46e89fcdceef779ace13e158dc43545a
Danila Pekhtserau <danila.pekhtserau@gmail.com>
2022-02-15 21:48:25 +0300

    US-2.2 Create company. Tests done.

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,5 @@
 
 class Company < ApplicationRecord
   has_many :users, dependent: :destroy
+  validates :name, presence: true, length: { in: 3..30 }
 end
--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :company do
     sequence(:name) { |i| "company_#{i}" }
+    name {'test'}
   end
 end
