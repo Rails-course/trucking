9a7e6f025c697d3fb169abeca612f404000c4708
Danila Pekhtserau <danila.pekhtserau@gmail.com>
2022-02-16 00:45:57 +0300

    TS7. Address model. ALMOUST Add spec.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,7 @@
 class Address < ApplicationRecord
   has_many :users, dependent: :nullify
+  validates :town, presence: true, length: { in: 3..30 }
+  validates :street, presence: true, length: { in: 3..30 }
+  validates :building, presence: true, numericality: { greater_than: 0 }
+  validates :apartment, presence: false, numericality: { greater_than: 0 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_14_122547) do
+ActiveRecord::Schema.define(version: 2022_02_15_211431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,9 +43,16 @@ ActiveRecord::Schema.define(version: 2022_02_14_122547) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.bigint "role_id"
+    t.string "first_name"
+    t.string "second_name"
+    t.string "middle_name"
+    t.date "birthday"
+    t.string "login"
+    t.bigint "company_id"
+    t.bigint "address_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["login"], name: "index_users_on_login", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
