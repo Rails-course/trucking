73fe1785af62cdbe160e825e6e95ae382469848b
Danila Pekhtserau <danila.pekhtserau@gmail.com>
2022-02-16 11:52:34 +0300

    US-2.2 Create company. Spec fixes.

--- spec/requests/companies_spec.rb
+++ spec/requests/companies_spec.rb
@@ -1,20 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe 'Companies', type: :request do
-  let(:company) { create(:company) }
+RSpec.describe "Companies", type: :request do
 
-  before do
+  let(:company) { create(:company, name: 'Company') }
+
+  before(:each) do
     sign_in create(:user)
   end
 
-  describe 'DELETE /companies' do
+  describe "DELETE /companies" do
     it 'valid delete' do
       delete "/companies/#{company.id}"
       expect(response).to have_http_status(204)
     end
     it 'invalid delete' do
-      delete "/companies/#{company.id + 1}"
+      delete "/companies/#{company.id+1}"
       expect(response).to have_http_status(404)
+
+    end
+  end
+
+  describe "CREATE /companies" do
+    it 'creates company' do
+      companyName = 'My comp'
+      post "/companies/create", params: { name: companyName }
+      expect(Company.last.name).to eq(companyName)
     end
   end
 end
