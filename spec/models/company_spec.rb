d29dc5d65c4a778879d3dc36c847caf9c73af007
Danila Pekhtserau <danila.pekhtserau@gmail.com>
2022-02-16 11:26:28 +0300

    US-2.2 Create company. Path and spec fixes.

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,24 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-
+  subject = build(:company)
   it 'should create Company' do
-    subject { build(:company) }
-    subject.name = 'Company'
     expect(subject.save).to eq(true)
   end
   it 'shouldn\'t create Company with < 3 chars name' do
-    subject { build(:company) }
     subject.name = 'er'
     expect(subject.save).to eq(false)
   end
   it 'shouldn\'t create Company with > 30 chars name' do
-    subject { build(:company) }
-    subject.name = 'testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest'
+    subject.name = 'a' * 31
     expect(subject.save).to eq(false)
   end
   it 'shouldn\'t create Company with empty name' do
-    subject { build(:company) }
     subject.name = ''
     expect(subject.save).to eq(false)
   end
