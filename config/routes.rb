8ba4beb5f0ba83977876d8c5f4d4ff266cf11235
NDragneelL9 <nell.dragneel@gmail.com>
2022-02-21 17:58:33 +0300

    hotfix/remove redundant route. Add companies link

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
   # match '*path', to: 'pages#home', via: :all
 
   scope '/companies' do
-    get '/new', to: 'companies#new_company'
     post '/create', to: 'companies#create_company'
   end
 end
