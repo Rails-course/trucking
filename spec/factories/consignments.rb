99a550e527077ea96d9a5b7fa45cffe90d711857
NDragneelL9 <nell.dragneel@gmail.com>
2022-02-23 21:32:53 +0300

    US-5.1.create-and-read-consignment. Create Consignment model specs

--- spec/factories/consignments.rb
+++ spec/factories/consignments.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :consignment do
-    
+    driver { create(:user_driver) }
+    dispatcher { create(:user_dispatcher) }
+    manager { create(:user_manager) }
+    association(:truck)
+    status { 'registered' }
+    consignment_seria { 'CS' }
+    sequence(:consignment_number) { |i| i.to_i }
+    bundle_seria { 'BS' }
+    sequence(:bundle_number) { |i| i.to_i }
   end
 end
