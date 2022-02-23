94dc8e5cde65aecfa1acc8cbdc8efbdcc6899346
NDragneelL9 <nell.dragneel@gmail.com>
2022-02-23 21:31:41 +0300

    US-5.1.create-and-read-consignment. delete wrong associations. remove env line from docker-compose yml

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,5 +1,4 @@
 class Destination < ApplicationRecord
-  has_many :consignments, dependent: :restrict_with_exception
   belongs_to :address
   validates :destination_name, presence: true, uniqueness: true, length: { in: 3..30 }
 end
--- app/models/goods_owner.rb
+++ app/models/goods_owner.rb
@@ -2,6 +2,5 @@
 
 class GoodsOwner < ApplicationRecord
   belongs_to :address
-  has_many :consignments, dependent: :restrict_with_exception
   validates :warehouse_name, presence: true, uniqueness: true, length: { in: 3..30 }
 end
