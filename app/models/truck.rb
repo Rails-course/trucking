6a640c072b99994cd9c48376e49945db876ef6a7
NDragneelL9 <nell.dragneel@gmail.com>
2022-03-01 18:03:35 +0300

    US-5.1.create-and-read-consignment. Change truck model validation, add index action to truck controller

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -2,5 +2,5 @@ class Truck < ApplicationRecord
   belongs_to :truck_type
   belongs_to :company
   validates :fuel_consumption, presence: true, numericality: { greater_than: 0 }
-  validates :truck_number, presence: true, numericality: { greater_than: 0 }
+  validates :truck_number, presence: true, length: { in: 3..30 }
 end
