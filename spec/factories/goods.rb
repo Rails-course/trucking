db6ff87a8748d124bdb3b4cae9fd13ad69f26d5a
NDragneelL9 <nell.dragneel@gmail.com>
2022-03-10 15:22:03 +0300

    US-5.1.create-and-read-consignment. Fix specs

--- spec/factories/goods.rb
+++ spec/factories/goods.rb
@@ -1,9 +1,10 @@
 FactoryBot.define do
   factory :good do
-    good_name{ 'Potato' }
-    quantity{ 12 }
-    unit_of_measurement{ 'item' }
-    status{ 'Not delivered' }
-    bundle_number{ 4 }
+    good_name { 'Potato' }
+    quantity { 12 }
+    unit_of_measurement { 'item' }
+    status { 'accepted' }
+    bundle_seria { 1 }
+    bundle_number { 4 }
   end
 end
