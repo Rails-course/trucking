b1c3e8a1e7a7660a52ced7b6efa6a45fd9c2bf61
NDragneelL9 <nell.dragneel@gmail.com>
2022-03-11 17:46:36 +0300

    US-5.1.create-and-read-consignment. update validations for goods and consignments. Update specs

--- app/models/good.rb
+++ app/models/good.rb
@@ -5,7 +5,7 @@ class Good < ApplicationRecord
   validates :unit_of_measurement, presence: true, length: { in: 2..15 }
   validates :status, presence: true, inclusion: { in: %w[accepted checked delivered lost] }
   validates :bundle_number, numericality: { greater_than: 0 }
-  validates :bundle_seria, presence: true, length: { in: 3..10 }
+  validates :bundle_seria, presence: true, length: { in: 2..10 }
 
   private
 
--- spec/factories/goods.rb
+++ spec/factories/goods.rb
@@ -3,8 +3,7 @@ FactoryBot.define do
     good_name { 'Potato' }
     quantity { 12 }
     unit_of_measurement { 'item' }
-    status { 'accepted' }
-    bundle_seria { 1 }
+    bundle_seria { 'BS' }
     bundle_number { 4 }
   end
 end
