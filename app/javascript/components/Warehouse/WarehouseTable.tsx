6eb9272d7db32ecc905fdad4d7604148ce9d2dc1
Kath Veremeychik <kateiiverem@gmail.com>
2022-03-24 10:11:22 +0300

    US-6.1 Create and read warehouse. Update data in the table

--- app/javascript/components/Warehouse/WarehouseTable.tsx
+++ app/javascript/components/Warehouse/WarehouseTable.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
+
 import {
-  List, ListItem, ListItemButton, ListItemText, Checkbox, IconButton, ListItemIcon,
+  List, ListItem, ListItemButton, ListItemText,
+  Checkbox, IconButton, ListItemIcon, Box,
 } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { pink } from '@mui/material/colors';
+
 import httpClient from '../../api/httpClient';
 
 interface WarehouseTableProps {
@@ -19,6 +22,15 @@ const WarehouseTable: React.FC<WarehouseTableProps> = (props: WarehouseTableProp
   const { warehouses, setWarehouses } = props;
   const [checked, setChecked] = React.useState([]);
 
+  const setWarehouseTrusted = async (id) => {
+    await httpClient.warehouses.trust(id);
+  };
+
+  const handleDeleteWarehouse = async (id) => {
+    await httpClient.warehouses.delete(id);
+    setWarehouses(warehouses.filter((data: warehouse) => data.id !== id));
+  };
+
   const handleToggle = (value: warehouse) => () => {
     const currentIndex = checked.indexOf(value);
     const newChecked = [...checked];
@@ -28,64 +40,66 @@ const WarehouseTable: React.FC<WarehouseTableProps> = (props: WarehouseTableProp
       newChecked.splice(currentIndex, 1);
     }
     setChecked(newChecked);
-  };
-
-  const handleDeleteWarehouse = async (id) => {
-    await httpClient.warehouses.delete(id);
-    setWarehouses(warehouses.filter((warehouse: warehouse) => warehouse.id !== id));
-  };
-
-  const setWarehouseTrusted = async (id) => {
-    await httpClient.warehouses.trust(id);
+    setWarehouseTrusted(value?.id).then();
   };
 
   if (!warehouses) return (<p>No data found...</p>);
 
   return (
-    <List sx={{
-      width: '100%', maxWidth: '70%', bgcolor: 'background.paper',
-    }}
-    >
-      {warehouses.map((value: warehouse) => {
-        const labelId = `checkbox-list-label-${value}`;
-
-        return (
-          <ListItem
-            key={value?.id}
-            secondaryAction={(
-              <IconButton
-                edge="end"
-                aria-label="delete"
-                onClick={() => handleDeleteWarehouse(value?.id)}
-              >
-                <DeleteIcon />
-              </IconButton>
-            )}
-            disablePadding
-          >
-            <ListItemButton role={undefined} onClick={handleToggle(value)} dense>
-              <ListItemIcon>
-                <Checkbox
-                  edge="start"
-                  checked={checked.indexOf(value) !== -1}
-                  onClick={() => setWarehouseTrusted(value?.id)}
-                  tabIndex={-1}
-                  disableRipple
-                  inputProps={{ 'aria-labelledby': labelId }}
-                  sx={{
-                    color: pink[800],
-                    '&.Mui-checked': {
-                      color: pink[600],
-                    },
-                  }}
-                />
-              </ListItemIcon>
-              <ListItemText id={labelId} primary={value?.warehouse_name} />
-            </ListItemButton>
-          </ListItem>
-        );
-      })}
-    </List>
+    <div>
+      <List sx={{
+        width: '100%', maxWidth: '70%', bgcolor: 'background.paper',
+      }}
+      >
+        <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between' }}>
+          <Box component="div" display="inline" p={1} m={1} bgcolor="background.paper">
+            Trusted
+          </Box>
+          <Box component="div" display="inline" p={1} m={1} bgcolor="background.paper">
+            Warehouse title
+          </Box>
+          <Box component="div" display="inline" p={1} m={1} bgcolor="background.paper" />
+        </div>
+        {warehouses.map((value: warehouse) => {
+          const labelId = `checkbox-list-label-${value}`;
+          return (
+            <ListItem
+              key={value?.id}
+              secondaryAction={(
+                <IconButton
+                  edge="end"
+                  aria-label="delete"
+                  onClick={() => handleDeleteWarehouse(value?.id)}
+                >
+                  <DeleteIcon />
+                </IconButton>
+                    )}
+              disablePadding
+              sx={{ width: '95%' }}
+            >
+              <ListItemButton role={undefined} onClick={handleToggle(value)} dense>
+                <ListItemIcon>
+                  <Checkbox
+                    edge="start"
+                    checked={checked.indexOf(value) !== -1 || value?.trusted}
+                    tabIndex={-1}
+                    disableRipple
+                    inputProps={{ 'aria-labelledby': labelId }}
+                    sx={{
+                      color: pink[800],
+                      '&.Mui-checked': {
+                        color: pink[600],
+                      },
+                    }}
+                  />
+                </ListItemIcon>
+                <ListItemText id={labelId} primary={value?.warehouse_name} />
+              </ListItemButton>
+            </ListItem>
+          );
+        })}
+      </List>
+    </div>
   );
 };
 
