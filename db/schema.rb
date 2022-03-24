2df268f7b934b2ea73b3385b8b54ceb216f0309b
NDragneelL9 <nell.dragneel@gmail.com>
2022-03-24 21:09:42 +0300

    bugfix/set default consignment status on DB level

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_23_160750) do
+ActiveRecord::Schema.define(version: 2022_03_24_174954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2022_03_23_160750) do
   end
 
   create_table "consignments", force: :cascade do |t|
-    t.string "status", null: false
+    t.string "status", default: "registered", null: false
     t.string "bundle_seria", default: "BS", null: false
     t.string "bundle_number", null: false
     t.string "consignment_seria", null: false
