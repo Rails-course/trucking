25d31729667162f11a296552dcca35ddb5bad49c
helmy-er <helbmerliuni@gmail.com>
2022-03-26 10:42:07 +0300

    US-5.6-Goods_conformity. BE+FE

---
+++ app/javascript/components/driver/WaybillGoods.tsx
@@ -0,0 +1,99 @@
+import * as React from 'react';
+import { Form, Formik } from 'formik';
+
+import {
+  Dialog, DialogActions, DialogContent, DialogTitle, Grid, List, Button,
+  ListItem, ListItemButton, ListItemIcon, ListItemText, Checkbox,
+} from '@mui/material';
+import httpClient from '../../api/httpClient';
+
+interface WaybillGoodsProps {
+  wayId: number;
+}
+
+const WaybillGoods: React.FC<WaybillGoodsProps> = (props: WaybillGoodsProps) => {
+  const wayId = props;
+  const [isActiveModal, setModalActive] = React.useState(false);
+  const [goods, setGoods] = React.useState([]);
+  const [checkedGoods, setCheckedGooods] = React.useState([]);
+
+  const handleGetGoods = () => {
+    if (wayId) {
+      setModalActive(true);
+      httpClient.goods.getWaybillGoods(wayId.wayId).then((response) => setGoods(response.data));
+    }
+  };
+  const handleClose = () => {
+    setModalActive(false);
+  };
+  const handleToggle = (value: number) => () => {
+    const currentIndex = checkedGoods.indexOf(value);
+    const newCheckedGoods = [...checkedGoods];
+    if (currentIndex === -1) {
+      newCheckedGoods.push(value);
+    } else {
+      newCheckedGoods.splice(currentIndex, 1);
+    }
+    setCheckedGooods(newCheckedGoods);
+  };
+
+  const handleSubmit = () => {
+    httpClient.goods.setWaybillGoodsStatus(wayId.wayId, checkedGoods);
+  };
+
+  return (
+    <div>
+      <Button onClick={() => { handleGetGoods(); }}>Open goods</Button>
+      <Dialog
+        open={isActiveModal}
+        onClose={handleClose}
+        sx={{ '& .MuiDialog-paper': { width: '80%', maxHeight: 535 } }}
+        maxWidth="xs"
+      >
+        <DialogTitle>Goods Conformity</DialogTitle>
+        <DialogContent>
+          <Grid container spacing={2} direction="column">
+            <Grid item xs={8}>
+              <Formik
+                initialValues={{ checked: [] }}
+                onSubmit={handleSubmit}
+              >
+                <Form>
+                  <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                    {goods.map((value) => {
+                      const labelId = `checkbox-list-label-${value}`;
+                      return (
+                        <ListItem
+                          key={value.id}
+                        >
+                          <ListItemButton role={undefined} onClick={handleToggle(value)} dense>
+                            <ListItemIcon>
+                              <Checkbox
+                                edge="start"
+                                checked={checkedGoods.indexOf(value) !== -1}
+                                tabIndex={-1}
+                                disableRipple
+                                inputProps={{ 'aria-labelledby': labelId }}
+                              />
+                            </ListItemIcon>
+                            <ListItemText id={labelId} primary={`${value?.good_name} ${value?.quantity} ${value?.unit_of_measurement}`} />
+                          </ListItemButton>
+                        </ListItem>
+                      );
+                    })}
+                  </List>
+                  <DialogActions>
+                    <Button type="submit" onClick={handleClose}>Submit</Button>
+                  </DialogActions>
+                </Form>
+              </Formik>
+            </Grid>
+          </Grid>
+        </DialogContent>
+      </Dialog>
+
+    </div>
+  );
+};
+
+export default WaybillGoods;
