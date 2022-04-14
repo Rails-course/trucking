0a871720c739192722144be82356131b21049f2b
helmy-er <helbmerliuni@gmail.com>
2022-04-14 23:23:10 +0300

    TS-21.Search-Component. Fix bugs

--- app/javascript/components/Search.tsx
+++ app/javascript/components/Search.tsx
@@ -1,9 +1,18 @@
 import * as React from 'react';
 import { Form, Formik } from 'formik';
-import {Button, Grid, Stack} from '@mui/material';
+import {Button, ButtonProps, Grid, Stack, styled} from '@mui/material';
 import { SearchProps } from '../common/interfaces_types';
 
 import FormikField from '../UI/FormikField';
+import { blue } from '@mui/material/colors';
+
+const ColorButton = styled(Button)<ButtonProps>(({ theme }) => ({
+    color: theme.palette.getContrastText(blue[500]),
+    backgroundColor: blue[500],
+    '&:hover': {
+        backgroundColor: blue[700],
+    },
+}));
 
 const Search: React.FC<SearchProps> = (props: SearchProps) => {
   const { setData, Data, searchField } = props;
@@ -17,17 +26,19 @@ const Search: React.FC<SearchProps> = (props: SearchProps) => {
     >
       <Form>
        <Grid  container spacing={2}>
-         <Grid item xs={8}>
+         <Grid item xs={11}>
           <FormikField
             name="text"
             label="searching field"
             required={false}
             type="text"
-            variant="standard"
+            color="success"
+            variant='standard'
+            margin="dense"
           />
          </Grid>
-         <Grid item xs={4}>
-          <Button type="submit" variant="outlined">Search</Button>
+         <Grid item xs={1}>
+          <ColorButton  type="submit" variant="contained">Search</ColorButton>
        </Grid>
        </Grid>
       </Form>
