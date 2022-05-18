20231784003f980bc3ef07beff270226641d7657
Pavel.Trusau <Helbmerliuni@gmail.com>
2022-05-18 15:26:31 +0300

    feature-LW-1-routes.revert changes

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class WarehousesController < ApplicationController
-  before_action :set_warehouse, only: %i[update destroy]
+  before_action :set_warehouse, only: %i[trust_warehouse destroy]
 
   def index
     @warehouses = Warehouse.all
