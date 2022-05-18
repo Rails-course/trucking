3285d3f66780b1e2393e338b5a16f11d735227ac
Pavel.Trusau <Helbmerliuni@gmail.com>
2022-05-18 16:12:40 +0300

    feature-LW-1-routes.revert changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,46 +4,52 @@ Rails.application.routes.draw do
   root 'pages#home'
   # User
   devise_for :users
-  get '/users', to: 'pages#users_index'
+
   scope '/users' do
+    get '', to: 'pages#users_index', as: 'users'
     post '/create', to: 'pages#create_user'
     get '/:id', to: 'pages#user_data'
     patch '/:id/edit', to: 'pages#update_user'
     delete '/:id', to: 'pages#destroy_user'
   end
 
- # Companies
-resources :companies
+  # Companies
+  resources :companies, except: :show
 
-# Consignment
-resources :consignments,only: %i[index create]
+  # Consignment
+  resources :consignments, only: %i[index create]
 
-patch 'consignment/:consignment_id/goods',to: "goods#update"
+  patch 'consignment/:consignment_id/goods', to: 'goods#update'
 
-resources :write_off_acts, only: %i[index create]
+  # Write off acts
+  resources :write_off_acts, only: %i[index create]
 
-resources :waybills ,except: :show
+  # Waybills
+  resources :waybills, except: :show
 
-resources :goods_owner,only: :index
+  # Goods owners
+  resources :goods_owner, only: :index
 
-resources :warehouses,except: :show
-patch '/warehouses/trust/:id',to: 'warehouses#trust_warehouse'
+  # warehouses
+  resources :warehouses, except: :show
+  patch '/warehouses/trust/:id', to: 'warehouses#trust_warehouse'
 
-patch '/checkpoints',to: 'checkpoints#update'
+  # Checkpoints
+  patch '/checkpoints', to: 'checkpoints#update'
 
-    # API
-namespace :api do
-      # V1 API DEPRECATED
-      # disable after demonstration
-  namespace :v1 do
-    resources :consignments, only: %i[index show] do
-    resources :consignment_goods, only: :index
-   end
-   resources :drivers, only: :index
-   resources :trucks, only: :index
-  end
-  namespace :v2 do
-    resources :consignments, only: %i[index show]
+  # API
+  namespace :api do
+    # V1 API DEPRECATED
+    # disable after demonstration
+    namespace :v1 do
+      resources :consignments, only: %i[index show] do
+        resources :consignment_goods, only: :index
+      end
+      resources :drivers, only: :index
+      resources :trucks, only: :index
+    end
+    namespace :v2 do
+      resources :consignments, only: %i[index show]
     end
   end
 end
