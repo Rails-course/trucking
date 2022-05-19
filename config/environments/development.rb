cf37233d014de0345b1f090e03ba84a11ea812a0
Catherine <60444425+MonkeyKingBar-bit@users.noreply.github.com>
2022-05-19 11:21:45 +0300

    fix: change url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,5 +58,5 @@ Rails.application.configure do
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
