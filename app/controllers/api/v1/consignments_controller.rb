f0d0b13044adda9d2b9b160ba6494341cabf604d
Pavel.Trusau <Helbmerliuni@gmail.com>
2022-06-06 15:11:07 +0300

    Feature.lw-7-pagination.fix

--- app/controllers/api/v1/consignments_controller.rb
+++ app/controllers/api/v1/consignments_controller.rb
@@ -7,7 +7,7 @@ module Api
         authorize! :read, Consignment
         excluded_columns = %w[created_at updated_at]
         consignment_api_columns = Consignment.attribute_names - excluded_columns
-        consignments,meta = paginate_collection(Consignment.select(consignment_api_columns))
+        consignments, meta = paginate_collection(Consignment.select(consignment_api_columns))
         render json: { consignments: consignments.to_json(include: included_params),
                        consignments_count: meta[:total_count] }
       end
--- app/controllers/api/v1/drivers_controller.rb
+++ app/controllers/api/v1/drivers_controller.rb
@@ -7,7 +7,8 @@ module Api
         authorize! :read, User
         excluded_columns = %w[created_at updated_at role_id address_id]
         drivers_api_columns = User.attribute_names - excluded_columns
-        drivers,meta = paginate_collection(User.select(drivers_api_columns).where(role: Role.find_by(role_name: 'driver')))
+        query = User.select(drivers_api_columns).where(role: Role.find_by(role_name: 'driver'))
+        drivers, meta = paginate_collection(query)
         render json: { drivers: drivers.to_json(include: [company: { only: :name }]),
                        drivers_count: meta[:total_count] }
       end
