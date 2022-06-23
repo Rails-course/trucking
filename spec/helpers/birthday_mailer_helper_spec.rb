657f3e4cf8c7e3d59d5b14b51555b423b8dbdd7d
Kath Veremeychik <kateiiverem@gmail.com>
2022-06-23 18:49:07 +0300

    feature/LW-25. Send birthday email. Add tests

--- spec/helpers/birthday_mailer_helper_spec.rb
+++ spec/helpers/birthday_mailer_helper_spec.rb
@@ -2,15 +2,12 @@ require 'rails_helper'
 
 class BirthdayMailerTest < ActionMailer::TestCase
   test "birthday" do
-    # Создайте email и сохраните его для будущих утверждений
     email = BirthdayMailer.birthday_email("friend@example.com", "Ivan Ivanovich Ivanov")
 
-    # Отправить письмо, затем проверить, что оно попало в очередь
     assert_emails 1 do
       email.deliver_now
     end
 
-    # Проверить тело отправленного письма, что оно содержит то, что мы ожидаем
     assert_equal ["trucking.app.reset@gmail.com"], email.from
     assert_equal ["friend@example.com"], email.to
     assert_equal "You have birthday email from trucking.app.reset@gmail.com", email.subject
