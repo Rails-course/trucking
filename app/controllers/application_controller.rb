1d3dade428c8c51e1cab91378ba3f9466ed202d8
Pavel.Trusau <Helbmerliuni@gmail.com>
2022-06-30 13:01:33 +0300

    Feature/l-48-errors. front

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,14 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::DeleteRestrictionError, with: :record_delete_error
   rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-
+  rescue_from ActionController::MethodNotAllowed, with: :method_is_not_allowed
   before_action :authenticate_user!
 
   private
+
+  def method_is_not_allowed
+    redirect_to error_405_path
+  end
 
   def record_not_found
     redirect_to error_404_path
@@ -24,7 +28,4 @@ class ApplicationController < ActionController::Base
     render json: exception.message, status: :method_not_allowed
   end
 
-  def access_denied(exception)
-    render json: exception.message, status: :method_not_allowed
-  end
 end
